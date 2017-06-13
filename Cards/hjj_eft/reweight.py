d541f533cfa5440e9de38fe918779258b89a8824
Philip <violatingcp@gmail.com>
2017-06-13 21:11:50 +0200

    fixing reweight following Marat's couplings

--- Cards/hjj_eft/reweight.py
+++ Cards/hjj_eft/reweight.py
@@ -13,14 +13,14 @@ def seq(start, stop, step=1):
     else:
         return([])
 
-def printsetg(gt,cgt,cyr,cyi,mtp):
-    weight_id = 'gt_%s_cgt_%s_cyr_%s_cyi_%s_mtp_%s' % (str(gt), str(cgt), str(cyr), str(cyi),str(mtp))
+def printsetg(cyr,cyi,cg,cgt,mtp):
+    weight_id = 'cyr_%s_cyi_%s_cg_%s_cgt_%s_mtp_%s' % (str(cyr), str(cyi), str(cg), str(cgt),str(mtp))
     print 'launch --rwgt_name=%s' % (weight_id.replace('.','p'))
     #Scalar
-    print 'set param_card DIM6  1  %s' % str(gt)
-    print 'set param_card DIM6  2  %s' % str(cgt)
-    print 'set param_card DIM6  3  %s' % str(cyr)
-    print 'set param_card DIM6  4  %s' % str(cyi)
+    print 'set param_card DIM6  1  %s' % str(cyr)
+    print 'set param_card DIM6  2  %s' % str(cyi)
+    print 'set param_card DIM6  3  %s' % str(cg)
+    print 'set param_card DIM6  4  %s' % str(cgt)
     print 'set param_card mass  6000006 %s' % str(mtp)
     print 'set param_card decay 25 AUTO'
     print 'set param_card decay 6000006 AUTO'
@@ -33,10 +33,13 @@ weight_counter = 0
 print '#' + str(weight_counter) + ' start of scalar'
 
 #pure scalar
-for mtp in [500,10000]:
-    for gt in [0.5,1.0,2.0,5.0]:
-        for cgt in [0.5,1.0,2.0,5.0]:
-            for cyr in [0.5,1.0,2.0,5.0]:
-                for cyi in [0.5,1.0,2.0,5.0]:
-                    printsetg(gt,cgt,cyr,cyi,mtp)
+for mtp in [10000]:
+    for cmean in [0.0,0.2,0.4,0.6,0.8,1.0]:
+        for cdiff in [-0.3,-0.15,0.0,0.15,0.3]:
+            for cyi in [0]:
+                for cgt in [0.]:
+                    cg=cmean+cdiff/2
+                    cyr=cmean-cdiff/2
+                    printsetg(cyr,cyi,cg,cgt,mtp)
                     weight_counter += 1
+
